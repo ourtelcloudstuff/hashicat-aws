a3664b27765ddd57a074a23ae81ee2d3bbebd23b
atj <atj.itt@gmail.com>
2021-01-21 22:25:40 +0000

    Backed fail

---
+++ s3-bucket.tf
@@ -0,0 +1,21 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
